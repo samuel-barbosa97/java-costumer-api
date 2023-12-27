a6ce7cdc6d0cd3c6e152914fa362f03c1995484c
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2023-12-27 16:29:30 -0300

    Refactor Application e Repository

--- src/main/java/io/github/barbosa/costumer/CostumerApplication.java
+++ src/main/java/io/github/barbosa/costumer/CostumerApplication.java
@@ -1,14 +1,17 @@
 package io.github.barbosa.costumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CostumerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CostumerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CostumerApplication.class, args);
-		System.out.println("Funcionando");
+		logger.info("Funcionando");
 	}
-
 }
+
